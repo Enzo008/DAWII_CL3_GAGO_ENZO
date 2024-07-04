796595dd2500fe05ba40c286417e922d0d74090d
Enzo008 <enzoaguirre629@gmail.com>
2024-07-03 19:30:16 -0500

    Fin Pregunta 3

--- src/app/dashboard/pregunta-3/image.service.ts
+++ src/app/dashboard/pregunta-3/image.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Image } from './Image';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  private urlApi = 'https://jsonplaceholder.typicode.com/photos';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerFotos(): Observable<Image[]> {
+    return this.http.get<Image[]>(this.urlApi).pipe(
+      map(fotos => fotos.filter((foto: Image) => 
+        foto.title.startsWith('a') || 
+        foto.title.startsWith('r') || 
+        foto.title.startsWith('s')
+      ))
+    );
+  }
 }
--- src/app/dashboard/pregunta-3/pregunta-3.component.ts
+++ src/app/dashboard/pregunta-3/pregunta-3.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image.service';
+import { Image } from './Image';
+import { MaterialModule } from '../../material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta-3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './pregunta-3.component.html',
   styleUrl: './pregunta-3.component.css'
 })
 export class Pregunta3Component {
+  fotos: Image[] = [];
 
+  constructor(private imageService: ImageService) {}
+
+  ngOnInit() {
+    this.imageService.obtenerFotos().subscribe((fotos: Image[]) => {
+      this.fotos = fotos;
+    });
+  }
 }
