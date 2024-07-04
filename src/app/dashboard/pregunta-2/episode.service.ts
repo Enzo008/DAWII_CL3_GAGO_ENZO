e23b58231c621f5d4248eb5bab2fe970c68240df
Enzo008 <enzoaguirre629@gmail.com>
2024-07-03 19:17:25 -0500

    Fin Pregunta 2

--- src/app/dashboard/pregunta-2/episode.service.ts
+++ src/app/dashboard/pregunta-2/episode.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Episode } from './Episode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EpisodeService {
+  private urlApi = 'https://rickandmortyapi.com/api/episode';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obtenerEpisodios(): Observable<Episode[]> {
+    return this.http.get<any>(this.urlApi).pipe(
+      map(response => response.results.filter((episode : Episode) => episode.id % 2 !== 0))
+    );
+  }
 }
--- src/app/dashboard/pregunta-2/pregunta-2.component.ts
+++ src/app/dashboard/pregunta-2/pregunta-2.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MaterialModule } from '../../material/material.module';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { EpisodeService } from './episode.service';
+import { Episode } from './Episode';
 
 @Component({
   selector: 'app-pregunta-2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule ],
   templateUrl: './pregunta-2.component.html',
   styleUrl: './pregunta-2.component.css'
 })
 export class Pregunta2Component {
+  displayedColumns: string[] = ['id', 'name', 'air_date', 'episode'];
+  dataSource = new MatTableDataSource<Episode>([]);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private episodeService: EpisodeService) {}
+
+  ngOnInit() {
+    this.episodeService.obtenerEpisodios().subscribe((episodios: Episode[]) => {
+      this.dataSource.data = episodios;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
